5bb2b059462211895ce04b5da611b697a8f38367
adarshkushawaha <udhabhaw@gmail.com>
2025-11-16 23:55:34 +0530

    Allow all hosts in production settings

--- LazyOne/settings.py
+++ LazyOne/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-9755rcl^sg+)6z4%8n(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
